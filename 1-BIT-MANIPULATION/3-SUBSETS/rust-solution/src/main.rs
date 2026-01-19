a8dcc01b032b134130b7fd1af35ec6c2fca97721
kudo07 <vknwl077@gmail.com>
2026-01-19 21:53:38 +0530

    feat:3-rust-sol

--- 1-BIT-MANIPULATION/3-SUBSETS/rust-solution/src/main.rs
+++ 1-BIT-MANIPULATION/3-SUBSETS/rust-solution/src/main.rs
@@ -1,3 +1,24 @@
+struct Solution;
+impl Solution {
+    pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let size_arr = nums.len();
+        let power_subsets = 1 << size_arr;
+        let mut ans: Vec<Vec<i32>> = vec![Vec::new(); power_subsets];
+        // vec![value;count]
+        // here Vec::new() create the empty array
+        for i in 0..size_arr {
+            for j in 0..power_subsets {
+                if (j >> i) & 1 == 1 {
+                    ans[j].push(nums[i])
+                }
+            }
+        }
+        ans
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arr: Vec<i32> = vec![1, 2, 3];
+    let result = Solution::subsets(arr);
+    println!("{:?}", result);
 }
